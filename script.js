cb03ff23101715646fe5e8418ac9ba21cadab6a8
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-05-31 18:50:35 +0530

    add

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 function convertToRoman(num) {
-  const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['I', 1]
-  };
+  const romanSymbols = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
   let romanNumeral = '';
 
-  for (let i = 0; i < Object.keys(obj).length; i++) {
-    const symbol = obj[i][0];
-    const value = obj[i][1];
-
-    // Repeat the symbol while subtracting its value from the number
-    while (num >= value) {
-      romanNumeral += symbol;
-      num -= value;
-    }
-
-    // Handle subtractive notation by checking if the current symbol can be combined with the next symbol
-    if (i % 2 === 0 && i < Object.keys(obj).length - 1) {
-      const nextSymbol = obj[i + 2][0];
-      const nextValue = obj[i + 2][1];
-
-      if (num >= value - nextValue) {
-        romanNumeral += nextSymbol + symbol;
-        num -= value - nextValue;
-      }
-    }
+  for (let i = 0; i < romanSymbols.length; i++) {
+	while (num >= romanSymbols[i][1]) {
+	  romanNumeral += romanSymbols[i][0];
+	  num -= romanSymbols[i][1];
+	}
   }
 
   return romanNumeral;
 }
 
-// console.log(convertToRoman(36)); // Output: XXXVI
+// Test cases
+console.log(convertToRoman(14)); // Output: XIV
+console.log(convertToRoman(798)); // Output: DCCXCVIII
+
+
+
+
+
+// do not edit below this line
+module.exports = convertToRoman
