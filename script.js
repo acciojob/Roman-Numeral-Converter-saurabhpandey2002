3688870f95e3db2b95c7110f6fda15deb4b27737
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-05-31 18:46:04 +0530

    ADD

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const obj = {
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
+  };
 
-  //your code here
-	let result = "";
+  let romanNumeral = '';
 
-  for (let key in obj) {
-    const [symbol, value] = obj[key];
-    const count = Math.floor(num / value);
+  for (let i = 0; i < Object.keys(obj).length; i++) {
+    const symbol = obj[i][0];
+    const value = obj[i][1];
 
-    if (count >= 1) {
-      result += symbol.repeat(count);
-      num -= value * count;
+    // Repeat the symbol while subtracting its value from the number
+    while (num >= value) {
+      romanNumeral += symbol;
+      num -= value;
     }
 
-    // Handle cases like 4, 40, 9, etc. (using subtraction notation)
-    if (key % 2 === 0 && num >= obj[key + 2][1] - value) {
-      result += obj[key + 2][0] + symbol;
-      num -= obj[key + 2][1] - value;
+    // Handle subtractive notation by checking if the current symbol can be combined with the next symbol
+    if (i % 2 === 0 && i < Object.keys(obj).length - 1) {
+      const nextSymbol = obj[i + 2][0];
+      const nextValue = obj[i + 2][1];
+
+      if (num >= value - nextValue) {
+        romanNumeral += nextSymbol + symbol;
+        num -= value - nextValue;
+      }
     }
   }
 
-  return result;
+  return romanNumeral;
 }
 
-// console.log(convertToRoman(36));
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// console.log(convertToRoman(36)); // Output: XXXVI
